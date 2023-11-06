5512be00dadf86b4aeb316e022e0356c42bb1cbf
NTnatan123 <148909264+NTnatan123@users.noreply.github.com>
2023-11-06 20:54:26 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400,400);
+  background("black");
+}
+
+function draw() {
+  stroke("light purple")
+  fill("purple");
+
+if(mouseIsPressed)  {
+  rect(mouseX,mouseY,20,20);
+}
+
+
+}
